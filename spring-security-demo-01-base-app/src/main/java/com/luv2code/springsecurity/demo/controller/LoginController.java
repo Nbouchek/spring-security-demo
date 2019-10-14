79b9759dca0834c721eace90f93270ab1eb40c8e
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-14 14:44:33 +0100

    Add bootstrap login form

--- spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
+++ spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
     @GetMapping("/showMyLoginPage")
     public String showMyLoginPage() {
-        return "plain-login";
+//        return "plain-login";
+        return "fancy-login";
     }
 }
